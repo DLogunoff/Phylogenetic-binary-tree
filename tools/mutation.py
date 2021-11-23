f07018495654fdab32b29b8102bd98f6976aeea9
Данила <danilalogunov13@gmail.com>
2021-11-23 18:07:34 +0300

    Mutations now changes from 1 to 40% of gens. Also return statement has been added.

--- tools/mutation.py
+++ tools/mutation.py
@@ -1,35 +1,36 @@
 from random import randint
 
 from tools.population_creator import (ImpossibleToCompleteError, Individual,
-                                      individual_create)
+                                      create_individual)
 
 
-def mutation(individual: Individual) -> None:
+def mutation(individual: Individual) -> Individual:
     """
 
     :param individual: особь
     :return: возвращает мутировавшую особь
 
     Для мутации хромосомы сначала с помощью генератора случайных чисел
-    выбирается количество генов, которые
-    не будут подвержены мутации (от 1 до половины длины хромосомы).
+    выбирается количество генов, которые не будут подвержены мутации.
+    Это значение лежит в интервале от 60% генов до N - 1 ген. То есть,
+    как минимум один ген мутирует всегда.
 
     После выбираются сами гены, которые не будут подтвержены мутации.
 
     На основании полученных в прошлом шаге номеров генов, формируется
-    шаблон хромосомы и функцией individual_create достраивается до
+    шаблон хромосомы и функцией create_individual достраивается до
     полной хромосомы.
     """
     length: int = len(individual)
     template: list = [None] * length
-    no_change_amount = range(randint(1, length // 2))
+    no_change_amount = range(randint(int(length * 0.6), length - 1))
     no_change_index = sorted(
         [randint(0, length - 1) for _ in no_change_amount]
     )
     for i in no_change_index:
         template[i] = individual[i]
     try:
-        mutant = individual_create(length + 1, template=template)
+        mutant = create_individual(length + 1, template=template)
     except ImpossibleToCompleteError:
         mutant = individual
-    individual[:] = mutant[:]
+    return mutant
