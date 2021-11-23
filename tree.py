53d8ea76be763ec7cb08316050416e7462119b4e
Данила <danilalogunov13@gmail.com>
2021-11-23 18:01:03 +0300

    Mutation and crossover now have 100% chance

--- tree.py
+++ tree.py
@@ -8,20 +8,16 @@ from tools.main_algorithm import genetic_algorithm
 INDIVIDUAL_SIZE = len(STANDARD)  # Количетсво генов в хромосове особи
 
 # Константы генетического алгоритма
-POPULATION_SIZE = 1000  # Количество особей в популяции
-P_MUTATION = 0.1  # Вероятность мутации
-P_CROSSOVER = 0.9  # Вероятность скрещивания
-MAX_GENERATIONS = 100  # Максимальное число поколений
-
+POPULATION_SIZE = 100  # Количество особей в популяции
+MAX_GENERATIONS = 5000  # Максимальное число поколений
 
 best_individual, min_values, mean_values = genetic_algorithm(
     speciman_size=INDIVIDUAL_SIZE,
     population_size=POPULATION_SIZE,
     max_generations=MAX_GENERATIONS,
-    p_crossover=P_CROSSOVER,
-    p_mutation=P_MUTATION,
 )
 
+draw_tree(best_individual)
 plt.plot(min_values, color='red')
 plt.plot(mean_values, color='blue')
 plt.xlabel('Поколение')
@@ -29,5 +25,3 @@ plt.ylabel('Мин/средняя приспособленность')
 plt.title(
     'Зависимость минимальной и средней приспособленности от поколения')
 plt.show()
-
-draw_tree(best_individual)
