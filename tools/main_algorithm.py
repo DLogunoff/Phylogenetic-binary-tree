40040eac78b14c576baad2de8f75fae48e2746f9
Данила <danilalogunov13@gmail.com>
2021-11-23 18:23:45 +0300

    Elitism feature and tournament selection has been deleted. Mutation and crossover now have 100% chance. Objects, which appeared as a result of crossover and mutation, now being added to the offspring population. Also initial population + objects, which appeared as a result of crossover are going to mutate. Tournament selection has been changed to simple type of selection, when to the next population goes only `POPULATION_SIZE` amount of the best individuals in term of their fitness value.

--- tools/main_algorithm.py
+++ tools/main_algorithm.py
@@ -1,15 +1,12 @@
-import random
-
 from tools.crossover import crossover
 from tools.fitness import fitness_count
 from tools.mutation import mutation
 from tools.population_creator import create_population
-from tools.selection import clone, select_tournament
+from tools.selection import clone
 
 
 def genetic_algorithm(speciman_size: int, population_size: int,
-                      max_generations: int, p_crossover: float,
-                      p_mutation: float) -> tuple:
+                      max_generations: int) -> tuple:
     """
 
     :param speciman_size: количество вершин
@@ -21,12 +18,11 @@ def genetic_algorithm(speciman_size: int, population_size: int,
 
     Стандартный генетический алгоритм: отбор - скрещивание - мутация.
 
-    Отбор - турнирный отбор (3 особи)
 
     Скрещивание - специально разработанный алгоритм для данной задачи.
     Подробнее описан в файле crossover.py
 
-    Мутация - случайно выбираются гены (до половины от общего числа),
+    Мутация - случайно выбираются гены (от 60% до N - 1 числа ген),
     остальные гены стираются и особь формируется на основе оставшихся
     генов.
 
@@ -39,26 +35,26 @@ def genetic_algorithm(speciman_size: int, population_size: int,
     min_fitness_values = []
     mean_fitness_values = []
     generation_counter = 0
-    elitism = False
     while generation_counter < max_generations:
         generation_counter += 1
-        if generation_counter >= int((1/2)*max_generations):
-            elitism = True
 
-        offspring = select_tournament(population, population_size)
-        offspring_copied = list(map(clone, offspring))
+        offspring_copied = list(map(clone, population))
         for parent1, parent2 in zip(
                 offspring_copied[::2], offspring_copied[1::2]):
-            if random.random() < p_crossover:
-                crossover(parent1, parent2, center, elitism)
-        for mutant in offspring_copied:
-            if random.random() < p_mutation:
-                mutation(mutant)
+            result = crossover(parent1, parent2, center)
+            offspring_copied.append(result[0])
+            offspring_copied.append(result[1])
+        for i in range(len(offspring_copied)):
+            offspring_copied.append(mutation(offspring_copied[i]))
         fresh_fitness_values = list(map(fitness_count, offspring_copied))
         for individual, fitness_value in zip(
                 offspring_copied, fresh_fitness_values):
             individual.fitness.values = fitness_value
-        population[:] = offspring_copied
+        best_offspring = sorted(
+            offspring_copied,
+            key=lambda ind: ind.fitness.values
+        )[:population_size]
+        population[:] = best_offspring[:]
 
         fitness_values = [ind.fitness.values for ind in population]
         min_fitness = min(fitness_values)
